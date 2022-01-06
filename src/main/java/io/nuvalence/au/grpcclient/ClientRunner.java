17374edaf285c04361873e5a1acab64ef4675b2c
Dan Ortega <ortega.dan2010@gmail.com>
2022-01-06 01:13:42 -0500

    adding client code

---
+++ src/main/java/io/nuvalence/au/grpcclient/ClientRunner.java
@@ -0,0 +1,32 @@
+package io.nuvalence.au.grpcclient;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.nuvalence.au.CarConfirmation;
+import io.nuvalence.au.CarInfo;
+import io.nuvalence.au.CarServiceGrpc;
+import io.nuvalence.au.CarServiceGrpc.CarServiceBlockingStub;
+
+/**
+ * Hello world!
+ *
+ */
+public class ClientRunner {
+    public static void main(String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080).usePlaintext().build();
+
+        CarServiceBlockingStub stub = CarServiceGrpc.newBlockingStub(channel);
+
+        CarInfo carInfo = CarInfo.newBuilder().setCarId(Long.parseLong(args[0])).setCarMessage(args[1]).build();
+
+        CarConfirmation carConfResponse = stub.carTalking(carInfo);
+
+        String serverResponseString = carConfResponse.getServerResponse();
+
+        System.out.println("Server response:");
+        System.out.println(serverResponseString);
+
+        channel.shutdown();
+
+    }
+}
