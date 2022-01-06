ff676dfc932b39d2ec9fd8b0455cf806b31c9142
Dan Ortega <ortega.dan2010@gmail.com>
2022-01-06 14:01:43 -0500

    starting to use logger

--- src/main/java/io/nuvalence/au/grpcserver/GrpcServerRunner.java
+++ src/main/java/io/nuvalence/au/grpcserver/GrpcServerRunner.java
@@ -2,10 +2,15 @@ package io.nuvalence.au.grpcserver;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 public class GrpcServerRunner {
+
+    private final static Logger logger = LoggerFactory.getLogger(GrpcServerRunner.class);
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
@@ -18,9 +23,9 @@ public class GrpcServerRunner {
 
         server.start();
 
-        System.out.println("******************************************");
-        System.out.println(" gRPC Server listening in port " + grpcport);
-        System.out.println("******************************************");
+        logger.info("******************************************");
+        logger.info(" gRPC Server listening in port " + grpcport);
+        logger.info("******************************************");
 
         server.awaitTermination();
 
