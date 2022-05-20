18cbe5dfc64d962c3372d2d378296041484ca898
Dan Ortega <ortega.dan2010@gmail.com>
2022-05-20 13:18:15 -0500

    adding interceptor method

--- src/main/java/io/nuvalence/au/grpcserver/LoggingInterceptor.java
+++ src/main/java/io/nuvalence/au/grpcserver/LoggingInterceptor.java
@@ -12,10 +12,10 @@ class LoggingInterceptor implements ServerInterceptor {
     @Override
     public <ReqT, RespT> Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> call, Metadata headers,
             ServerCallHandler<ReqT, RespT> next) {
-
+                
         System.out.println("*****************");
-        System.out.println(call.getAttributes().get(Grpc.TRANSPORT_ATTR_REMOTE_ADDR));
-        System.out.println(call.getAttributes().get(Grpc.TRANSPORT_ATTR_SSL_SESSION));
+        System.out.println(call.getAttributes().get(Grpc.TRANSPORT_ATTR_REMOTE_ADDR).toString().split(":")[0].substring(1));
+        System.out.println(call.getMethodDescriptor().getFullMethodName());
         System.out.println("*****************");
 
         return next.startCall(call, headers);
